3e6ef236e8c744d348553d4c857bc9ddf1bc8246
Gage Lewis <glewis.ghl@gmail.com>
2022-08-31 06:49:47 +0900

    form basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FormFooter from "./FormFooter.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +17,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <FormFooter />
     </div>
   );
 }
