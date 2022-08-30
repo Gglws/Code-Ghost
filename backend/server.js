97eb1f8f40a8db5bb7d762121ed74209ff3a7f67
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-08-30 13:46:50 -0500

    Migration.sql and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,42 @@
+import express from "express";
+import pg from "pg";
+import { readFile } from "fs/promises";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+dotenv.config();
 const PORT = 3000;
+// const { DATABASE_URL, NODE_ENV, PORT} = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public')); 
+app.use(cors());
+
+const pool = new pg.Pool({
+    database: 'fec'
+    // connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+});
+
+app.get("/api", (req, res) => {
+    readFile("public/index.html", "utf-8").then(string => {
+        res.send(string);
+    });
+});
+
+app.get("/api/member", (req, res) => {
+    pool.query('SELECT * FROM memberInfo').then((data) => {
+        res.send(data.rows);
+    });
+});
+
+app.use((err, req, res, next)=> {
+    res.sendStatus(500);
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
