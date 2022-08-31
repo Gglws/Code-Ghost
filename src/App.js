4c4b95cd014c790f7307d9b9c07e1b909e3967c3
Robin Kluis <faultic@gmail.com>
2022-08-31 09:13:54 -0500

    commit before merge test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Daeyeon from "./robin/profile highlights/Daeyeon.js";
 import Gage from "./robin/profile highlights/Gage.js";
 import Jeremy from "./robin/profile highlights/Jeremy.js";
 import Robin from "./robin/profile highlights/Robin.js";
-import "./app.css";
+import "./styles.css";
 import FormFooter from "./FormFooter.js";
 //IMPORT SECTION END
 
