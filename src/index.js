8dd61df6f0cbf8479e916e94459ad456e5bcdda2
Robin Kluis <faultic@gmail.com>
2022-08-31 09:03:50 -0500

    renamed css file for smoothness

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.js';
-import './app.css'
+import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
