2c82d995030bf4479bfa7498ff5350e234e137d3
Gage Lewis <glewis.ghl@gmail.com>
2022-09-03 05:32:18 +0900

    profile changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,46 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Header from "../robin/Header.js";
 
-import Intro from '../robin/Intro.js';
-import ProfileHighlights from '../ProfileHighlights.js';
-import PlaceholderSqlBox from '../PlaceholderSqlBox.js';
-import FormFooter from '../FormFooter.js';
+import Intro from "../robin/Intro.js";
+import ProfileHighlights from "../ProfileHighlights.js";
+import PlaceholderSqlBox from "../PlaceholderSqlBox.js";
+import FormFooter from "../FormFooter.js";
 import { motion, useScroll } from "framer-motion";
-import LoginForm from '../LoginForm.js';
+import LoginForm from "../LoginForm.js";
 
 export default function Home() {
-    const { scrollYProgress } = useScroll();
-    return (
-        <section>
-            <Header />
-            <h2>TEAM BIBBLE</h2>
-            <ul>
-                <li>
-                    <Link to='/about' className='nav-btn'>Go to About</Link>
-                </li>
-                <li>
-                    <Link to='/login' className='nav-btn'>Go to Login</Link>
-                </li>        
-            </ul>
-            <div>
-                <div className="App">
-                    <motion.div className="progress-bar" style={{ scaleX: scrollYProgress }}/>
-                </div>
-                <div className="innerApp">
-                    <Intro scroll={scrollYProgress}/>
-                    <ProfileHighlights />
-                    <PlaceholderSqlBox />
-                </div>
-            </div> 
-            <LoginForm />
-            <FormFooter />
-        </section>
-    )
-  }
+  const { scrollYProgress } = useScroll();
+  return (
+    <section>
+      <Header />
+      <h2>TEAM BIBBLE</h2>
+      <ul>
+        <li>
+          <Link to="/about" className="nav-btn">
+            Go to About
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="nav-btn">
+            Go to Login
+          </Link>
+        </li>
+      </ul>
+      <div>
+        <div className="App">
+          <motion.div
+            className="progress-bar"
+            style={{ scaleX: scrollYProgress }}
+          />
+        </div>
+        <div className="innerApp">
+          <Intro scroll={scrollYProgress} />
+          <ProfileHighlights scroll={scrollYProgress} />
+          <PlaceholderSqlBox />
+        </div>
+      </div>
+      <LoginForm />
+      <FormFooter />
+    </section>
+  );
+}
--- src/profile_highlights/Daeyeon.js
+++ src/profile_highlights/Daeyeon.js
@@ -1,9 +1,56 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import profilePic from "./images.jpg";
+import $ from "jquery";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue,
+} from "framer-motion";
 
-function Daeyeon() {
+function Daeyeon(props) {
+  const [scroller, setScroller] = useState(0);
+
+  let count = 0;
+  let count1 = 1;
+  let count2 = 1;
+  let count3 = 100;
+  let tracker = 0;
+
+  useEffect(() => {
+    props.scroll.onChange(() => {
+      count++;
+
+      //change to use scroll y progress
+      if (props.scroll.get() > tracker) {
+        count2 += 1;
+      } else if (props.scroll.get() < tracker) {
+        count2 -= 1;
+      }
+
+      $(".daeyeonProfile").css({
+        "background-image": `linear-gradient(30deg,#214079 1%, #663a7e ${count2}%, #a82b7d 100%)`,
+      });
+
+      tracker = props.scroll.get();
+      setScroller(count);
+    });
+  }, [props.scroll, count]);
+  // props.scroll.onChange(() => {
+  //   count++;
+
+  //   // console.log(props.scroll);
+  //   // $(".profile").css({
+  //   //   "background-image": "linear-gradient(30, rgba(255, 255, 255)",
+  //   // });
+  //   // $(".profile").css({ height: `${newCount}` });
+
+  //   // console.log(count);
+  // });
+
   return (
-    <div className="profile">
+    <motion.div className="daeyeonProfile">
       <div className="profileHeader"> Daeyeon </div>
       <div className="profileContent">Blah blah blah blah blah blah</div>
       <img
@@ -13,7 +60,7 @@ function Daeyeon() {
         width="500"
         height="500"
       />
-    </div>
+    </motion.div>
   );
 }
 
