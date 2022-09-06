8f28ed60a143c97e705ef928c4061cdf8557f21b
Gage Lewis <glewis.ghl@gmail.com>
2022-09-07 07:40:39 +0900

    end of day

--- src/robin/Header.js
+++ src/robin/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-// import HeaderMenu from "./HeaderMenu.js";
+import HeaderMenu from "./HeaderMenu.js";
 import { motion } from "framer-motion";
 // import Form from "../Form.js";
 import CG from "./CG.PNG";
-
 
 function Header() {
   return (
