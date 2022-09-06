2bde92a4d710b4a3da0680633a0b9ea15de59c58
kelemii <98933496+kelemii@users.noreply.github.com>
2022-09-06 09:40:07 -0700

    hosting static

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-app.use(express.static("/..public"));
+app.use(express.static("../build"));
 app.use(cors());
 
 const pool = new pg.Pool({
