d4aece542e0757c926901f68b09ed2d342243b80
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-09-07 11:34:47 -0500

    Get backend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import cors from 'cors';
 
 dotenv.config();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 // const { DATABASE_URL, NODE_ENV, PORT} = process.env;
 
 const app = express();
