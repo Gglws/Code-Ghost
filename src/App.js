699990c57071847efe8f097359380e1ef63fafc5
Gage Lewis <glewis.ghl@gmail.com>
2022-09-09 07:22:31 +0900

    end of day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 //IMPORT SECTION START
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login.js";
 import Home from "./components/Home.js";
 import About from "./components/About.js";
+import Messages from "./components/Messages.js";
 import React from "react";
 import "./styles.css";
 
 function App() {
   return (
-
     <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />}/> 
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
     </Router>
   );
 }
--- src/robin/HeaderMenu.js
+++ src/robin/HeaderMenu.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './HeaderMenu.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HeaderMenu.css";
 
 function HeaderMenu() {
   return (
-    <div className='page-nav-container'>
-      <div className='page-nav-btn'>
+    <div className="page-nav-container">
+      <div className="page-nav-btn">
         <div>
-          <Link to='/' className='nav-btn-login'>HOME</Link>
+          <Link to="/" className="nav-btn-login">
+            HOME
+          </Link>
         </div>
         <div>
-          <Link to='/about' className='nav-btn-about'>ABOUT</Link>
+          <Link to="/about" className="nav-btn-about">
+            ABOUT
+          </Link>
         </div>
         <div>
-          <Link to='/login' className='nav-btn-login'>LOGIN</Link>
+          <Link to="/login" className="nav-btn-login">
+            LOGIN
+          </Link>
+        </div>
+        <div>
+          <Link to="/messages" className="nav-btn-login">
+            MESSAGES (TEMP)
+          </Link>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default HeaderMenu
+export default HeaderMenu;
