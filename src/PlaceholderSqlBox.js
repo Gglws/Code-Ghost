1a3704b877bd421ab90c6359cc7d0c517d5ef4c6
Robin Kluis <faultic@gmail.com>
2022-09-09 10:33:46 -0500

    start

--- src/PlaceholderSqlBox.js
+++ src/PlaceholderSqlBox.js
@@ -17,15 +17,15 @@ function PlaceholderSqlBox() {
           <img src={daeyeon} className="sqlSubBoxPic" alt="Profile Pic" />
           <h3 className="sqlBoxName">Daeyeon Cho</h3>
           <p className="bulletPoint">JavaScript (ES5/ES6)</p>
-          <p className="bulletPoint">JQuery</p>
-          <p className="bulletPoint">HTML5/CSS</p>
+          <p className="bulletPointLesser">JQuery</p>
+          <p className="bulletPointLesser">HTML5/CSS</p>
           <p className="bulletPoint">Express</p>
           <p className="bulletPoint">PostgresSQL</p>
           <p className="bulletPoint">Node.js</p>
           <p className="bulletPoint">React</p>
-          <p className="bulletPoint">Git</p>
-          <p className="bulletPoint">RESTful API</p>
-          <p className="bulletPoint">Heroku/Render Deployment</p>
+          <p className="bulletPointLesser">Git</p>
+          <p className="bulletPointLesser">RESTful API</p>
+          <p className="bulletPointLesser">Heroku/Render Deployment</p>
           <a
             href="https://www.linkedin.com/in/daeyeon-cho-20a503225/"
             className="button"
@@ -38,14 +38,14 @@ function PlaceholderSqlBox() {
           <h3 className="sqlBoxName">Gage Lewis</h3>
           <p className="bulletPoint">JavaScript (ES5/ES6)</p>
           <p className="bulletPoint">JQuery</p>
-          <p className="bulletPoint">HTML5/CSS</p>
+          <p className="bulletPointLesser">HTML5/CSS</p>
           <p className="bulletPoint">Express</p>
-          <p className="bulletPoint">PostgresSQL</p>
-          <p className="bulletPoint">Microsoft Visual Basic(VB.NET/VBA)</p>
+          <p className="bulletPointLesser">PostgresSQL</p>
+          <p className="bulletPoint">Microsoft Visual Basic<br/>(VB.NET/VBA)</p>
           <p className="bulletPoint">React</p>
           <p className="bulletPoint">Java</p>
-          <p className="bulletPoint">RESTful API</p>
-          <p className="bulletPoint">Heroku/Render Deployment</p>
+          <p className="bulletPointLesser">RESTful API</p>
+          <p className="bulletPointLesser">Heroku/Render Deployment</p>
           <a href="https://www.linkedin.com/in/gage-lewis1/" className="button">
             LinkedIn
           </a>
@@ -55,11 +55,11 @@ function PlaceholderSqlBox() {
           <h3 className="sqlBoxName">Jeremy Linder</h3>
           <p className="bulletPoint">JavaScript (ES5/ES6)</p>
           <p className="bulletPoint">JQuery</p>
-          <p className="bulletPoint">HTML5/CSS</p>
+          <p className="bulletPointLesser">HTML5/CSS</p>
           <p className="bulletPoint">Express</p>
           <p className="bulletPoint">PostgresSQL</p>
           <p className="bulletPoint">Node.js</p>
-          <p className="bulletPoint">React</p>
+          <p className="bulletPointLesser">React</p>
           <p className="bulletPoint">Git</p>
           <p className="bulletPoint">RESTful API</p>
           <p className="bulletPoint">Heroku/Render Deployment</p>
